c706ec0b7db9028026622537c648584c181352d4
viniciusxande30 <vinicius.xande30@gmail.com>
2024-03-23 14:34:24 -0300

    Enviar Atualizações

--- index.php
+++ index.php
@@ -38,6 +38,45 @@
     
 </head>
 
+
+<?php
+
+function enviar_email($destinatario, $assunto, $mensagem) {
+    if (empty($destinatario) || empty($assunto) || empty($mensagem)) {
+        return false;
+    }
+
+    $remetente = "rsfreelas@gmail.com"; 
+    $headers = "From: $remetente\r\n";
+    $headers .= "Reply-To: $remetente\r\n";
+    $headers .= "Content-Type: text/html; charset=UTF-8\r\n";
+
+    if (mail($destinatario, $assunto, $mensagem, $headers)) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $nome = $_POST['name'];
+    $nome = $_POST['email'];
+    $destinatario = $_POST["phone"];
+    $assunto = $_POST["subject"];
+    $mensagem = $_POST["message"];
+
+    // Enviar e-mail
+    if (enviar_email($destinatario, $assunto, $mensagem)) {
+        echo "E-mail enviado com sucesso!";
+    } else {
+        echo "Erro ao enviar o e-mail. Por favor, tente novamente.";
+    }
+}
+
+?>
+
+
+
 <body>
     <style>
     html {
@@ -557,7 +596,7 @@
                             <img src="assets/images/resources/contact-image.jpg" alt="">    
                         </div>
                         <div class="contact-form">
-                            <form  action="mail.php" method="post">
+                            <form  action="<?php echo $_SERVER["PHP_SELF"]; ?>" method="post">
                                 <div class="row">
                                     <div class="col-xl-6">
                                         <div class="input-box"> 
