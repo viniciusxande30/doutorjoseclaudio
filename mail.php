b83ded3f55dbc76f2321f930fc12c1fd96424214
viniciusxande30 <vinicius.xande30@gmail.com>
2024-03-23 21:33:08 -0300

    Enviar Atualizações

--- mail.php
+++ mail.php
@@ -16,7 +16,7 @@ $mail = new PHPMailer(true);
 try {
     //Server settings
     $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
-    $mail->isSMTP();                                            //Send using SMTP
+    //$mail->isSMTP();                                            //Send using SMTP
     $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
     $mail->Username   = 'doutorjoseclaudioadv@gmail.com';                     //SMTP username
