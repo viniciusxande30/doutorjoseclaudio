6f61820e5be25eac56065b50e1bd52c178b65eb2
viniciusxande30 <vinicius.xande30@gmail.com>
2024-03-23 21:30:13 -0300

    Enviar Atualizações

--- mail.php
+++ mail.php
@@ -17,22 +17,22 @@ try {
     //Server settings
     $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
     $mail->isSMTP();                                            //Send using SMTP
-    $mail->Host       = 'smtp.rsweb.com.br';                     //Set the SMTP server to send through
+    $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-    $mail->Username   = 'comercial2@rsweb.com.br';                     //SMTP username
-    $mail->Password   = 'nisexandi2';                               //SMTP password
-    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;            //Enable implicit TLS encryption
-    $mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+    $mail->Username   = 'doutorjoseclaudioadv@gmail.com';                     //SMTP username
+    $mail->Password   = 'Resultado@2024rsweb';                               //SMTP password
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
+    $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
     //Recipients
-    $mail->setFrom('comercial2@rsweb.com.br', 'José Claudio');
-    $mail->addAddress('comercial2@rsweb.com.br', 'José Claudio');     //Add a recipient
-    $mail->addReplyTo('comercial2@rsweb.com.br', 'José Claudio');
+    $mail->setFrom('doutorjoseclaudioadv@gmail.com', 'José Claudio');
+    $mail->addAddress('doutorjoseclaudioadv@gmail.com', 'José Claudio');     //Add a recipient
+    $mail->addReplyTo('doutorjoseclaudioadv@gmail.com', 'José Claudio');
 
     //Content
     $mail->isHTML(true);                                  //Set email format to HTML
     $mail->Subject = 'Mensagem via Site';
-    $body = "Teste de envio de E-mail";
+    $body = "Nome: ".$_POST['name']."<br>";
     $mail->Body    = $body;
     // $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
 
