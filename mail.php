a4e1e42ca9e18ef6b5c3df3c0a971b5677cb26d5
viniciusxande30 <vinicius.xande30@gmail.com>
2024-03-23 22:09:54 -0300

    Enviar Atualizações

--- mail.php
+++ mail.php
@@ -12,13 +12,10 @@ if(isset($_POST['enviar'])){
 
 //Create an instance; passing `true` enables exceptions
 $mail = new PHPMailer(true);
-
-
-try {
     // Definir o cabeçalho Content-Type para UTF-8
     $mail->CharSet = 'UTF-8';
     //Server settings
-    $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+    //$mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
     $mail->isSMTP();                                            //Send using SMTP
     $mail->Host       = 'rsweb.com.br';                     //Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
@@ -35,14 +32,10 @@ try {
     //Content
     $mail->isHTML(true);                                  //Set email format to HTML
     $mail->Subject = 'Doutor José Claudio - Cotação';
-    $body = "Nome: ".$_POST['name']."<br>"."E-mail: ".$_POST['e-mail']."<br>"."Telefone: ".$_POST['phone']."<br>"."Subject: ".$_POST['subject']."<br>".$_POST['message']."<br>";
+    $body = "Nome: ".$_POST['name']."<br>"."E-mail: ".$_POST['email']."<br>"."Telefone: ".$_POST['phone']."<br>"."Subject: ".$_POST['subject']."<br>".$_POST['message']."<br>";
     $mail->Body    = $body;
     // $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
 
     $mail->send();
     echo 'E-mail enviado com sucesso';
-} catch (Exception $e) {
-    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
-}
-    
 }
