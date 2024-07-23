f9ba00dfa16fcf4badfd0c9bfbaa9d6104df06d9
viniciusxande30 <vinicius.xande30@gmail.com>
2024-07-22 22:07:03 -0300

    Commit upload servidor

--- index.php
+++ index.php
@@ -35,6 +35,13 @@
         <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
         <script src="assets/js/html5shiv.js"></script>
     <![endif]-->
+    <!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-KC7KN4R5');</script>
+<!-- End Google Tag Manager -->
     
 </head>
 
@@ -42,6 +49,10 @@
 
 
 <body>
+    <!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KC7KN4R5"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
     <style>
     html {
         scroll-behavior: smooth;
